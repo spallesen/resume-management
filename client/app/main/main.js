727cadc2722ea077d51fec01f304788a0e6ca557
spallesen <sorenpallesen@gmail.com>
2015-04-13 07:22:52 +0200

    first commit

---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('pabloApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
