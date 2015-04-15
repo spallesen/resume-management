8fa1772c197ca0cbc1e10762ff2419a68f6b5b1f
spallesen <sorenpallesen@gmail.com>
2015-04-15 21:56:15 +0200

    First real

--- client/app/profile/profile.controller.js
+++ client/app/profile/profile.controller.js
@@ -8,7 +8,45 @@ angular.module('pabloApp')
  $scope.schema = 
  {type: "object", title: "Comment", required: ["comments", "projects"],
   properties: {
-      projects: {title:"Project", type:"string"},
+      title: {title:"Title", type:"string"},
+      surname: {type: "string", title:'Family name'},
+      forename: {type: "string", title:'First name'}, 
+
+
+      addressline1: {type: 'string', title: 'Address'},      
+      addressline2: {type: 'string', title: 'Address'},           
+      addressline4: {type: 'string', title: 'Address'},
+      city: {type: 'string', title: 'City'},          
+      postcode: {type: 'string', title: 'Postcode'},
+      country: {type: 'string', title: 'Country'},
+
+      phone: {type: 'string', title: 'Phone'},
+      eMail: {type: 'string', title: 'E-Mail'},
+      availabilityFromDate: {type: 'string', title: 'Availablility date'},
+      agent: {type: 'string', title: 'Your agent'},
+      terms: {type: 'boolean', title: 'Distribution accept'},
+      terms2: {type: 'boolean', title: 'Web site accept'},
+
+      homePhone: {type: 'string', title: 'Home Phone'},
+      skype: {type: 'string', title: 'Skype ID'},
+      dateOfBirth: {type: 'string', title:'Date of Birth'},
+      maritalStatus: {type: 'string', enum:['Married','Divorced','Single'], title: 'Marital Status'},
+      showMarialStatusOnCv: {type: 'boolean', enum: ['Yes', 'No'], title: 'Show Marital Status on CV'},
+      ItExperienceSince: {type: 'string', title: 'IT Experience Since'},
+      drivelLicense: {type: 'boolean', title: 'Have driver license'},
+      car: {type: 'boolean', title: 'Have car'},
+      customerClause: {type: 'boolean', title: 'Have Customer Clause'},
+      competitionClause: {type: 'boolean', title: 'Have competitive clause'},
+
+      title: {type: 'string', title: 'Title'},
+      consultantCategory: {type: 'string', enum:['.NET','Java','Agile','BI', 'Business modelling','CA Gen'], title: 'Consultant Categories'},
+      personalProfile: {type: 'string', title: 'Personal Profile'},
+      workExperience: {type: 'string', title: 'Work Experience'},
+      primaryApplications: {type: 'string', title: 'Personal networks'},
+      otherInformation: {type: 'string', title: 'otherInformation'},
+
+
+
       comments: {"type": "array", "title": "Hay why", "maxItems": 2, "items": {"type": "object", "properties": {
           "name": {"title": "Name", "type": "string", "description": "Yo please tell us your name", "validationMessage": "Hey you - whats your name?"},
           "email": {"title": "Email", "type": "string", "pattern": "^\\S+@\\S+$", "description": "Email will be used for evil."},
@@ -28,20 +66,11 @@ angular.module('pabloApp')
 };
 
 $scope.form = 
+[
+  "*"
+];
 
 /*[
-      {
-    "type": "help",
-    "helpvalue": "<h4>Please add as many resumes as you want</h4><p>You can also reorder by drag'n'drop.</p>"
-  },"*",
-  {
-    "type": "submit",
-    "style": "btn-danger",
-    "title": "Save"
-  }
-]*/
-
-[
   {"type": "help",
   "helpvalue": "<h1>Array Example</h1><p>Try adding a couple of forms, reorder by drag'n'drop.</p>"
   },
@@ -94,7 +123,7 @@ $scope.form =
     "style": "btn-info",
     "title": "OK"
   }
-]
+]*/
 
 
 
